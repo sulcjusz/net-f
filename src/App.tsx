c371010aa046bb8c65c0a5420f44395906a0aeb7
sulcjusz <sulcjusz1@gmail.com>
2024-03-12 10:06:09 +0100

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import HomePage from './components/pages/HomePage'
 import BaseLayout from './components/layouts/BaseLayout'
